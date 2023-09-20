3c56dbc7e02b46df79169b2b0f4b48794944f16b
marc <sed@protonmail.com>
2023-09-20 13:06:00 +0200

    remove checking for ens

--- dapps/retropool-v2/src/components/ConnectWallet.js
+++ dapps/retropool-v2/src/components/ConnectWallet.js
@@ -7,11 +7,9 @@ export default function ConnectWallet() {
 
   useEffect(() => {
     if (wallet?.provider) {
-      const { name, avatar } = wallet?.accounts[0].ens ?? {};
       setAccount({
         address: wallet.accounts[0].address,
         balance: wallet.accounts[0].balance,
-        ens: { name, avatar: avatar?.url },
       });
     }
   }, [wallet]);
